aa4a3b9a4bb078095ee4ce18e54bd41277d5ed27
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-11-04 19:33:50 -0500

    mult_list

---
+++ mult_list.py
@@ -0,0 +1,13 @@
+#multiply all the numbers in a list
+
+def mult_list(lists):
+    result = 1
+    for x in lists:
+        result = result * x
+    return result
+
+list = [2, 3, 4]
+list2 = [9, 9, 9, 9]
+
+print(mult_list(list))
+print(mult_list(list2))
